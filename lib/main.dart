827fd47f6c339dba5ed82a5daf10d4c3958e2b57
Buddy Jonte <mail@jpjonte.de>
2024-05-25 23:47:21 +0200

    Test render

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,115 +15,160 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'NES',
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: const EmulatorWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// NES resolution: 256x224 (NTSC) or 256x240 (PAL)
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class EmulatorWidget extends StatelessWidget {
+  const EmulatorWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: FutureBuilder(
+        future: _loadImage(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final image = snapshot.data;
+
+          if (image == null) {
+            return const Center(child: Text('Failed to load image'));
+          }
+
+          return CustomPaint(
+            painter: EmulatorPainter(image: image),
+            child: const SizedBox.expand(),
+          );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<ui.Image> _loadImage() async {
+    final pixels = List.filled(256 * 224, 0xFF000000);
+
+    void setPixel(int x, int y, int color) {
+      final index = x + y * 256;
+      pixels[index] = color;
+    }
+
+    setPixel(0, 0, 0xFFFFFFFF);
+    setPixel(1, 0, 0xFFFFFFFF);
+    setPixel(2, 0, 0xFFFFFFFF);
+    setPixel(1, 1, 0xFFFFFFFF);
+    setPixel(1, 2, 0xFFFFFFFF);
+    setPixel(1, 3, 0xFFFFFFFF);
+    setPixel(1, 4, 0xFFFFFFFF);
+
+    setPixel(4, 0, 0xFFFFFFFF);
+    setPixel(5, 0, 0xFFFFFFFF);
+    setPixel(6, 0, 0xFFFFFFFF);
+    setPixel(4, 1, 0xFFFFFFFF);
+    setPixel(4, 2, 0xFFFFFFFF);
+    setPixel(5, 2, 0xFFFFFFFF);
+    setPixel(6, 2, 0xFFFFFFFF);
+    setPixel(4, 3, 0xFFFFFFFF);
+    setPixel(4, 4, 0xFFFFFFFF);
+    setPixel(5, 4, 0xFFFFFFFF);
+    setPixel(6, 4, 0xFFFFFFFF);
+
+    setPixel(8, 0, 0xFFFFFFFF);
+    setPixel(9, 0, 0xFFFFFFFF);
+    setPixel(10, 0, 0xFFFFFFFF);
+    setPixel(8, 1, 0xFFFFFFFF);
+    setPixel(8, 2, 0xFFFFFFFF);
+    setPixel(9, 2, 0xFFFFFFFF);
+    setPixel(10, 2, 0xFFFFFFFF);
+    setPixel(10, 3, 0xFFFFFFFF);
+    setPixel(8, 4, 0xFFFFFFFF);
+    setPixel(9, 4, 0xFFFFFFFF);
+    setPixel(10, 4, 0xFFFFFFFF);
+
+    setPixel(12, 0, 0xFFFFFFFF);
+    setPixel(13, 0, 0xFFFFFFFF);
+    setPixel(14, 0, 0xFFFFFFFF);
+    setPixel(13, 1, 0xFFFFFFFF);
+    setPixel(13, 2, 0xFFFFFFFF);
+    setPixel(13, 3, 0xFFFFFFFF);
+    setPixel(13, 4, 0xFFFFFFFF);
+
+    final pixelData = Uint8List.fromList(
+      pixels
+          .expand(
+            (value) => [
+              (value >> 16) & 0xFF,
+              (value >> 8) & 0xFF,
+              value & 0xFF,
+              (value >> 24) & 0xFF,
+            ],
+          )
+          .toList(),
+    );
+
+    final buffer = await ui.ImmutableBuffer.fromUint8List(pixelData);
+
+    final descriptor = ui.ImageDescriptor.raw(
+      buffer,
+      width: 256,
+      height: 224,
+      pixelFormat: ui.PixelFormat.rgba8888,
+    );
+
+    final codec = await descriptor.instantiateCodec();
+
+    final frame = await codec.getNextFrame();
+
+    return frame.image;
+  }
+}
+
+class EmulatorPainter extends CustomPainter {
+  EmulatorPainter({required this.image});
+
+  final ui.Image image;
+
+  final Paint backgroundPaint = Paint()..color = Colors.black;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(Offset.zero & size, backgroundPaint);
+
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final scale = max(1, min(size.width ~/ 256, size.height ~/ 224));
+
+    final topLeft = center - const Offset(128, 112) * scale.toDouble();
+
+    final matrix = Matrix4.identity().scaled(scale.toDouble())
+      ..setTranslationRaw(topLeft.dx, topLeft.dy, 0);
+
+    final paint = Paint()
+      ..shader = ImageShader(
+        image,
+        TileMode.decal,
+        TileMode.decal,
+        matrix.storage,
+      );
+
+    canvas.drawRect(
+      topLeft & Size(256 * scale.toDouble(), 224 * scale.toDouble()),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
