e9b4e30224bfddbf821391aeee8888473b91f9b4
Buddy Jonte <mail@jpjonte.de>
2024-08-18 15:18:49 +0200

    Refactor address modes

--- lib/nes/cpu/address_mode.dart
+++ lib/nes/cpu/address_mode.dart
@@ -1,123 +1,153 @@
-import 'package:nesd/extension/hex_extension.dart';
 import 'package:nesd/nes/bus.dart';
 import 'package:nesd/nes/cpu/cpu.dart';
 
-typedef AddressReader = (int, bool) Function(CPU, int);
-typedef AddressModeDebugger = String Function(CPU, int, List<int>, int);
+sealed class AddressMode {
+  (int, bool) read(CPU cpu, int pc);
 
-class AddressMode {
-  AddressMode(this.read, this.operandCount, this.debug);
-
-  final AddressReader read;
-  final int operandCount;
-  final AddressModeDebugger debug;
+  int get operandCount;
 }
 
 bool wasPageCrossed(int from, int to) => from & 0xff00 != to & 0xff00;
 
-final implicit = AddressMode(
-  (cpu, pc) => (addressNone, false),
-  0,
-  (cpu, pc, operands, address) => '',
-);
-final accumulator = AddressMode(
-  (cpu, pc) => (addressA, false),
-  0,
-  (cpu, pc, operands, address) => 'A',
-);
-final immediate = AddressMode(
-  (cpu, pc) => (pc, false),
-  1,
-  (cpu, pc, operands, address) => '#\$${operands[0].toHex()}',
-);
-final zeroPage = AddressMode(
-  (cpu, pc) => (cpu.read(pc) & 0xff, false),
-  1,
-  (cpu, pc, operands, address) => '\$${operands[0].toHex()}',
-);
-final zeroPageX = AddressMode(
-  (cpu, pc) => ((cpu.read(pc) + cpu.X) & 0xff, false),
-  1,
-  (cpu, pc, operands, address) =>
-      '\$${operands[0].toHex()},X @ ${address.toHex()}',
-);
-final zeroPageY = AddressMode(
-  (cpu, pc) => ((cpu.read(pc) + cpu.Y) & 0xff, false),
-  1,
-  (cpu, pc, operands, address) =>
-      '\$${operands[0].toHex()},Y @ ${address.toHex()}',
-);
-final relative = AddressMode(
-  (cpu, pc) {
+class Implicit extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => (addressNone, false);
+
+  @override
+  int get operandCount => 0;
+}
+
+class Accumulator extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => (addressA, false);
+
+  @override
+  int get operandCount => 0;
+}
+
+class Immediate extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => (pc, false);
+
+  @override
+  int get operandCount => 1;
+}
+
+class ZeroPage extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => (cpu.read(pc) & 0xff, false);
+
+  @override
+  int get operandCount => 1;
+}
+
+class ZeroPageX extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => ((cpu.read(pc) + cpu.X) & 0xff, false);
+
+  @override
+  int get operandCount => 1;
+}
+
+class ZeroPageY extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => ((cpu.read(pc) + cpu.Y) & 0xff, false);
+
+  @override
+  int get operandCount => 1;
+}
+
+class Relative extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
     final offset = cpu.read(pc);
     final offsetSigned = offset >= 0x80 ? offset - 0x100 : offset;
-
     return (pc + 1 + offsetSigned, false);
-  },
-  1,
-  (cpu, pc, operands, address) => '\$${address.toHex(width: 4)}',
-);
-final absolute = AddressMode(
-  (cpu, pc) => (cpu.read16(pc), false),
-  2,
-  (cpu, pc, operands, address) => '\$${address.toHex(width: 4)}',
-);
-final absoluteX = AddressMode(
-  (cpu, pc) {
-    final address = cpu.read16(pc);
-    final targetAddress = address + cpu.X;
+  }
 
+  @override
+  int get operandCount => 1;
+}
+
+class Absolute extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) => (cpu.read16(pc), false);
+
+  @override
+  int get operandCount => 2;
+}
+
+class AbsoluteX extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
+    final address = cpu.read16(pc);
+    final targetAddress = (address + cpu.X) & 0xffff;
     return (targetAddress, wasPageCrossed(address, targetAddress));
-  },
-  2,
-  (cpu, pc, operands, address) =>
-      '\$${cpu.read16(pc).toHex(width: 4)},X @ ${address.toHex(width: 4)}',
-);
-final absoluteY = AddressMode(
-  (cpu, pc) {
+  }
+
+  @override
+  int get operandCount => 2;
+}
+
+class AbsoluteY extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
     final address = cpu.read16(pc);
     final targetAddress = (address + cpu.Y) & 0xffff;
-
     return (targetAddress, wasPageCrossed(address, targetAddress));
-  },
-  2,
-  (cpu, pc, operands, address) =>
-      '\$${cpu.read16(pc).toHex(width: 4)},Y' ' @ ${address.toHex(width: 4)}',
-);
-final indirect = AddressMode(
-  (cpu, pc) {
+  }
+
+  @override
+  int get operandCount => 2;
+}
+
+class Indirect extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
     final address = cpu.read16(pc);
     final targetAddress = cpu.read16(address, wrap: true);
-
     return (targetAddress, false);
-  },
-  2,
-  (cpu, pc, operands, address) =>
-      '(\$${operands[1].toHex()}${operands[0].toHex()}) ='
-      ' ${address.toHex(width: 4)}',
-);
-final indexedIndirect = AddressMode(
-  (cpu, pc) {
+  }
+
+  @override
+  int get operandCount => 2;
+}
+
+class IndexedIndirect extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
     final address = (cpu.read(pc) + cpu.X) & 0xff;
     final targetAddress = cpu.read16(address, wrap: true);
-
     return (targetAddress, false);
-  },
-  1,
-  (cpu, pc, operands, address) => '(\$${operands[0].toHex()},X)'
-      ' @ ${((cpu.read(pc) + cpu.X) & 0xff).toHex()}'
-      ' = ${address.toHex(width: 4)}',
-);
-final indirectIndexed = AddressMode(
-  (cpu, pc) {
+  }
+
+  @override
+  int get operandCount => 1;
+}
+
+class IndirectIndexed extends AddressMode {
+  @override
+  (int, bool) read(CPU cpu, int pc) {
     final zeroPageAddress = cpu.read(pc);
     final address = cpu.read16(zeroPageAddress, wrap: true);
     final targetAddress = (address + cpu.Y) & 0xffff;
-
     return (targetAddress, wasPageCrossed(address, targetAddress));
-  },
-  1,
-  (cpu, pc, operands, address) => '(\$${operands[0].toHex()}),Y'
-      ' = ${cpu.read16(cpu.read(pc), wrap: true).toHex(width: 4)}'
-      ' @ ${address.toHex(width: 4)}',
-);
+  }
+
+  @override
+  int get operandCount => 1;
+}
+
+final implicit = Implicit();
+final accumulator = Accumulator();
+final immediate = Immediate();
+final zeroPage = ZeroPage();
+final zeroPageX = ZeroPageX();
+final zeroPageY = ZeroPageY();
+final relative = Relative();
+final absolute = Absolute();
+final absoluteX = AbsoluteX();
+final absoluteY = AbsoluteY();
+final indirect = Indirect();
+final indexedIndirect = IndexedIndirect();
+final indirectIndexed = IndirectIndexed();
