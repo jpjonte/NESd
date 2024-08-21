3ee11804c99019d9f0defb9d0e4f19ab72979ee9
Buddy Jonte <mail@jpjonte.de>
2024-08-21 19:44:12 +0200

    Improve test coverage

--- test/nestest_test.dart
+++ test/nestest_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nesd/nes/bus.dart';
 import 'package:nesd/nes/cartridge/cartridge.dart';
 import 'package:nesd/nes/event/event_bus.dart';
 import 'package:nesd/nes/nes.dart';
@@ -13,26 +14,42 @@ void main() {
 
     final cartridge = Cartridge.fromFile(path, file.readAsBytesSync());
 
-    final nes = NES(cartridge: cartridge, eventBus: EventBus())
-      ..reset()
-      ..cpu.PC = 0xc000
-      ..cpu.cycles = 7
-      ..ppu.cycle = 21;
+    final nes = NES(cartridge: cartridge, eventBus: EventBus())..reset();
 
-    while (true) {
-      final exit = nes.cpu.PC == 0xc66e;
+    nes.bus.buttonDown(0, NesButton.start); // start tests of official opcodes
 
-      nes.step();
+    _runLoop(nes, 0xc0ea); // run until all tests passed
 
-      final low = nes.cpu.read(0x02);
-      final high = nes.cpu.read(0x03);
-      final result = (high << 8) | low;
+    nes.bus.buttonUp(0, NesButton.start);
 
-      expect(result, equals(0));
+    nes.bus.buttonDown(0, NesButton.select); // switch to next page
 
-      if (exit) {
-        break;
-      }
-    }
+    _runLoop(nes, 0xc135); // run until page has been switched
+
+    nes.bus.buttonUp(0, NesButton.select);
+
+    nes.bus.buttonDown(0, NesButton.start); // start tests of unofficial opcodes
+
+    _runLoop(nes, 0xc18f); // run until all tests passed
   });
+}
+
+void _runLoop(NES nes, int breakAddress) {
+  while (true) {
+    final exit = nes.cpu.PC == breakAddress;
+
+    nes.step();
+
+    nes.apu.sampleIndex = 0;
+
+    final low = nes.cpu.read(0x02);
+    final high = nes.cpu.read(0x03);
+    final result = (high << 8) | low;
+
+    expect(result, equals(0));
+
+    if (exit) {
+      break;
+    }
+  }
 }
