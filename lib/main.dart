6c65a55878b412c809aed0bae5228bc9e8df873a
Buddy Jonte <mail@jpjonte.de>
2025-03-31 07:34:52 +0200

    Migrate SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,22 @@ import 'package:nesd/ui/settings/shared_preferences.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/util/legacy_to_async_migration_util.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  const sharedPreferencesOptions = SharedPreferencesOptions();
+
   final preferences = await SharedPreferences.getInstance();
   final packageInfo = await PackageInfo.fromPlatform();
   final applicationSupport = await getApplicationSupportDirectory();
+
+  await migrateLegacySharedPreferencesToSharedPreferencesAsyncIfNecessary(
+    legacySharedPreferencesInstance: preferences,
+    sharedPreferencesAsyncOptions: sharedPreferencesOptions,
+    migrationCompletedKey: 'migrationCompleted',
+  );
 
   final fileSystem =
       Platform.isAndroid ? AndroidSafFileSystem() : NativeFileSystem();
