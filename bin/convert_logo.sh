e5bcb82c70f654e16dac05abcc1c6979a1444df4
Buddy Jonte <mail@jpjonte.de>
2025-04-13 19:24:53 +0200

    #101 Fix logo on android

--- bin/convert_logo.sh
+++ bin/convert_logo.sh
@@ -2,4 +2,7 @@
 
 name=$1
 
+color=$(magick "$name".svg -colorspace srgb -format "%[hex:p{512,512}]" info:)
+
 magick -background none +antialias "$name".svg "$name".png
+magick -background "#$color" +antialias "$name".svg "$name-adaptive".png
