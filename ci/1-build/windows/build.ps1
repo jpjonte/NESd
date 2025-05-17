8b32cfc5f0bc8d398b4d4173de2d4a44419e842d
Buddy Jonte <nesd@jpj.dev>
2025-05-17 11:20:10 +0200

    Fix releases

--- ci/1-build/windows/build.ps1
+++ ci/1-build/windows/build.ps1
@@ -3,12 +3,18 @@ flutter build windows --release
 $lz4library = ".\windows\eslz4-win64.dll"
 $buildDirectory = ".\build\windows\x64\runner\Release"
 
-if (-not (Test-Path -Path $buildDirectory)) {
-    New-Item -ItemType Directory -Path $buildDirectory -Force
-}
+New-Item -ItemType Directory -Path $buildDirectory -Force
 
 if (Test-Path $lz4library) {
     Copy-Item -Path $lz4library -Destination $buildDirectory
 } else {
     Write-Host "File does not exist: $lz4library"
 }
+
+New-Item -ItemType Directory -Path ".\dist" -Force
+
+$artifactPath = Join-Path -Path ".\dist" -ChildPath "$env:ARTIFACT.windows-x64"
+
+New-Item -ItemType Directory -Path $artifactPath -Force
+
+Get-ChildItem $buildDirectory | Copy-Item -Destination $artifactPath -Recurse
--- ci/3-release/zip_windows_artifacts.sh
+++ ci/3-release/zip_windows_artifacts.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-mv artifacts/"$ARTIFACT".windows-x64.zip artifacts/"$ARTIFACT".windows-x64
-
 zip -r artifacts/"$ARTIFACT".windows-x64.zip artifacts/"$ARTIFACT".windows-x64
 
 rm -rf artifacts/"$ARTIFACT".windows-x64
