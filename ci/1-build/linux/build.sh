9dd8ad5f992a39c838d321157435abbb87b95f36
Buddy Jonte <nesd@jpj.dev>
2025-10-03 22:41:55 +0200

    Restructure repo: App now lives in packages/nesd/

--- ci/1-build/linux/build.sh
+++ ci/1-build/linux/build.sh
@@ -2,9 +2,15 @@
 
 set -eux
 
+repo_root=$(git rev-parse --show-toplevel)
+
 sudo apt-get update -y
 sudo apt-get install -y ninja-build libgtk-3-dev
+
+pushd "$repo_root/packages/nesd" >/dev/null
 
 mkdir -p build/linux/"$ARCH"/release/bundle/
 
 flutter build linux --release --target-platform=linux-"$ARCH"
+
+popd >/dev/null
