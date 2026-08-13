ed1b0e9b01dfa43ad5d870d48cd74eb6dfa58b80
Buddy Jonte <nesd@jpj.dev>
2026-08-13 16:52:31 +0200

    Add display tool end-to-end test

--- packages/nesd/test/ui/menu/tools_screen_test.dart
+++ packages/nesd/test/ui/menu/tools_screen_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nesd/ui/emulator/nes_controller.dart';
+import 'package:nesd/ui/emulator/tools/display_tool.dart';
 import 'package:nesd/ui/emulator/tools/emulator_tool.dart';
 import 'package:nesd/ui/emulator/tools/emulator_tools_controller.dart';
+import 'package:nesd/ui/menu/menu_screen.dart';
 
 import '../robot.dart';
 
@@ -41,6 +43,45 @@ void main() {
     expect(r.container.read(emulatorToolsControllerProvider), isEmpty);
 
     await r.goBack();
+    await r.menuScreen.tapQuitGame();
+    await r.waitUntil(() => r.container.read(nesStateProvider) == null);
+  });
+
+  testWidgets('display tool opens from the Tools screen and shows over the '
+      'game', (tester) async {
+    final r = Robot(tester)
+      ..initSettings({
+        'recentRoms': [
+          {
+            'file': {
+              'path': '/test/roms/nestest.nes',
+              'name': '/test/roms/nestest.nes',
+              'type': 'file',
+            },
+          },
+        ],
+      });
+
+    await r.pumpApp();
+    await r.mainMenu.tapFirstRomTile();
+
+    await r.emulator.tapMenu();
+    await r.menuScreen.tapTools();
+
+    await r.tools.tapTool(EmulatorTool.display);
+
+    expect(r.container.read(emulatorToolsControllerProvider), {
+      EmulatorTool.display,
+    });
+
+    await r.goBack();
+    await r.menuScreen.tapResume();
+
+    expect(find.byType(DisplayToolWidget), findsOneWidget);
+
+    await r.waitUntil(() => find.byType(MenuScreen).evaluate().isEmpty);
+
+    await r.emulator.tapMenu();
     await r.menuScreen.tapQuitGame();
     await r.waitUntil(() => r.container.read(nesStateProvider) == null);
   });
