0cdc8cbca7d4c4f947d8beeb2845213799af2657
Buddy Jonte <nesd@jpj.dev>
2026-08-31 15:46:46 +0200

    Add test ROMs that already pass

---
+++ packages/nesd/test/test_roms/blargg_status_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'rom_robot.dart';
+
+const _base = '../../roms/test';
+
+const _passing = <String>[
+  'instr_test-v5/all_instrs.nes',
+  'instr_timing/instr_timing.nes',
+  'instr_misc/rom_singles/01-abs_x_wrap.nes',
+  'instr_misc/rom_singles/02-branch_wrap.nes',
+  'cpu_interrupts_v2/rom_singles/1-cli_latency.nes',
+  'mmc3_test/1-clocking.nes',
+  'mmc3_test/5-MMC3.nes',
+];
+
+void main() {
+  for (final rom in _passing) {
+    test(rom, () {
+      final result = RomRobot('$_base/$rom').runUntilResult();
+
+      expect(result.passed, isTrue, reason: result.toString());
+    });
+  }
+}
