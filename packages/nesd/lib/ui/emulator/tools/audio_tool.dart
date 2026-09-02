a72dd0e64afccd5ef2e79d22ab7d14f8e03b30d9
Buddy Jonte <nesd@jpj.dev>
2026-09-02 17:10:23 +0200

    #370 Add mixer to the Audio tool

--- packages/nesd/lib/ui/emulator/tools/audio_tool.dart
+++ packages/nesd/lib/ui/emulator/tools/audio_tool.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nesd/ui/common/settings_section_header.dart';
 import 'package:nesd/ui/settings/audio/low_pass_filter_switch.dart';
+import 'package:nesd/ui/settings/audio/mixer_sliders.dart';
 import 'package:nesd/ui/settings/audio/swap_duty_cycles_switch.dart';
 import 'package:nesd/ui/settings/audio/volume_slider.dart';
 
@@ -10,7 +12,13 @@ class AudioToolWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [VolumeSlider(), LowPassFilterSwitch(), SwapDutyCyclesSwitch()],
+      children: [
+        VolumeSlider(),
+        LowPassFilterSwitch(),
+        SwapDutyCyclesSwitch(),
+        SettingsSectionHeader(title: 'Mixer'),
+        MixerSliders(),
+      ],
     );
   }
 }
