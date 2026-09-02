95b9448101c18d1762cce0c265f6101dd6a87256
Buddy Jonte <nesd@jpj.dev>
2026-09-02 15:49:33 +0200

    #370 Mix APU channels

--- packages/nesd/lib/nes/apu/apu_mix.dart
+++ packages/nesd/lib/nes/apu/apu_mix.dart
@@ -1,3 +1,23 @@
+import 'package:nesd/nes/apu/mixer_settings.dart';
+
+@pragma('vm:prefer-inline')
+double mixSample({
+  required int pulse1,
+  required int pulse2,
+  required int triangle,
+  required int noise,
+  required int dmc,
+  required double expansion,
+  required MixerSettings mixer,
+}) {
+  final pulseIndex = pulse1 * mixer.pulse1 + pulse2 * mixer.pulse2;
+
+  final tndIndex =
+      3 * triangle * mixer.triangle + 2 * noise * mixer.noise + dmc * mixer.dmc;
+
+  return pulseMix(pulseIndex) + tndMix(tndIndex) + expansion;
+}
+
 @pragma('vm:prefer-inline')
 double pulseMix(double index) => 95.52 / (8128.0 / index + 100);
 
