c90467dad634030a03c21457635f730926fa7c72
Buddy Jonte <nesd@jpj.dev>
2026-09-07 19:56:28 +0200

    Add a controller for the paginated grid

--- packages/nesd/lib/ui/common/paginated_grid.dart
+++ packages/nesd/lib/ui/common/paginated_grid.dart
@@ -4,7 +4,10 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nesd/ui/common/focus_first_descendant.dart';
+import 'package:nesd/ui/common/paginated_grid_controller.dart';
 import 'package:nesd/ui/common/rom_tile.dart';
+import 'package:nesd/ui/emulator/input/intents.dart';
 
 const _gutterWidth = 40.0;
 
@@ -13,17 +16,36 @@ class PaginatedGrid extends HookConsumerWidget {
     this.tileWidth = gameTileWidth,
     this.tileHeight = gameTileHeight,
     this.children = const [],
+    this.controller,
     super.key,
   });
 
   final List<Widget> children;
+
+  final PaginatedGridController? controller;
 
   final double tileWidth;
   final double tileHeight;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final page = useState(0);
+    final ownController = usePaginatedGridController();
+
+    final pageController = controller ?? ownController;
+
+    useListenable(pageController);
+
+    final tilesFocusNode = useFocusNode();
+
+    useEffect(() {
+      void focusNewPage() => WidgetsBinding.instance.addPostFrameCallback(
+        (_) => focusFirstDescendant(tilesFocusNode),
+      );
+
+      pageController.addListener(focusNewPage);
+
+      return () => pageController.removeListener(focusNewPage);
+    }, [pageController]);
 
     final mediaQuery = MediaQuery.of(context);
 
@@ -45,7 +67,9 @@ class PaginatedGrid extends HookConsumerWidget {
         // at least one page, even with no children
         final pages = count > 0 ? max(1, (children.length / count).ceil()) : 1;
 
-        final currentPage = page.value.clamp(0, pages - 1);
+        pageController.pageCount = pages;
+
+        final currentPage = pageController.page;
 
         final romPaths = children
             .skip(currentPage * count)
@@ -54,42 +78,83 @@ class PaginatedGrid extends HookConsumerWidget {
 
         final visibleRows = romPaths.slices(columnCount).toList();
 
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              width: gutter,
-              height: visibleRows.length * tileHeight,
-              child: currentPage > 0
-                  ? InkWell(
-                      onTap: () => page.value = currentPage - 1,
-                      child: const Icon(Icons.arrow_back_ios),
-                    )
-                  : const SizedBox(),
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                for (final row in visibleRows)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: row,
-                  ),
-              ],
-            ),
-            SizedBox(
-              width: gutter,
-              height: visibleRows.length * tileHeight,
-              child: currentPage < pages - 1
-                  ? InkWell(
-                      onTap: () => page.value = currentPage + 1,
-                      child: const Icon(Icons.arrow_forward_ios),
-                    )
-                  : const SizedBox(),
-            ),
-          ],
+        return PaginatedGridActions(
+          controller: pageController,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: gutter,
+                height: visibleRows.length * tileHeight,
+                child: currentPage > 0
+                    ? InkWell(
+                        onTap: pageController.previousPage,
+                        child: const Icon(Icons.arrow_back_ios),
+                      )
+                    : const SizedBox(),
+              ),
+              Focus(
+                focusNode: tilesFocusNode,
+                canRequestFocus: false,
+                skipTraversal: true,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (final row in visibleRows)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: row,
+                      ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: gutter,
+                height: visibleRows.length * tileHeight,
+                child: currentPage < pages - 1
+                    ? InkWell(
+                        onTap: pageController.nextPage,
+                        child: const Icon(Icons.arrow_forward_ios),
+                      )
+                    : const SizedBox(),
+              ),
+            ],
+          ),
         );
       },
     );
   }
+}
+
+class PaginatedGridActions extends StatelessWidget {
+  const PaginatedGridActions({
+    required this.controller,
+    required this.child,
+    super.key,
+  });
+
+  final PaginatedGridController controller;
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => Actions(
+    actions: {
+      PreviousTabIntent: CallbackAction<PreviousTabIntent>(
+        onInvoke: (_) {
+          controller.previousPage();
+
+          return null;
+        },
+      ),
+      NextTabIntent: CallbackAction<NextTabIntent>(
+        onInvoke: (_) {
+          controller.nextPage();
+
+          return null;
+        },
+      ),
+    },
+    child: child,
+  );
 }
