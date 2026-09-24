431fa5bdfee2017f6861953517dcd559a50061f8
Buddy Jonte <nesd@jpj.dev>
2026-09-24 09:08:37 +0200

    Only handle APU frame counter ends if counter is high enough

--- packages/nesd/lib/nes/apu/frame_counter/frame_counter.dart
+++ packages/nesd/lib/nes/apu/frame_counter/frame_counter.dart
@@ -157,6 +157,8 @@ class FrameCounter {
     } else if (counter == _quarter2) {
       _clockQuarterFrame();
       _clockHalfFrame();
+    } else if (counter < _fourStepEnd - 2) {
+      return;
     } else if (fiveStep) {
       _stepFiveStepEnd();
     } else {
